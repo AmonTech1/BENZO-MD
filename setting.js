f20d3c21939cf1445e9f780eade63f08f626c499
Amon <itz.3.kid@gmail.com>
2026-08-01 00:04:33 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -4,8 +4,8 @@
 module.exports = {
   BOT_NAME: process.env.BOT_NAME || 'BENZO MD', 
   OWNER_NAME: process.env.OWNER_NAME || 'Amon', 
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '254759006509', 
-  SESSION_ID: process.env.SESSION_ID || 'blinder~iXhgQASb#vG1LCcVDGhlCy3CvyOcXOep4c3A8jHq-6skMLrj-WbY', // paste here your session ID
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '', 
+  SESSION_ID: process.env.SESSION_ID || '', // paste here your session ID
   MODE: process.env.MODE || "private", // public or private
   TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
 };
